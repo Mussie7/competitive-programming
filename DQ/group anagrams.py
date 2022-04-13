6762ff4b899a493586715768823faef6f0a1bcfa
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-04-13 20:46:14 +0300

    Update group anagrams.py

--- DQ/group anagrams.py
+++ DQ/group anagrams.py
@@ -1,10 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram = {}
+        strs2 = []
         for i in strs:
-            if "".join(sorted(i)) in anagram:
-                anagram["".join(sorted(i))].append(i)
-            else:
-                anagram["".join(sorted(i))] = [i]
+            strs2.append(["".join(sorted(i)), i])
+        strs2.sort()
+        ans = []
+        while len(strs2) > 0:
+            temp = strs2.pop(0)
+            ans.append([temp[1]])
+            for i in range(len(strs2)):
+                if temp[0] == strs2[0][0]:
+                    temp = strs2.pop(0)
+                    ans[-1].append(temp[1])
+                else:
+                    break
         
-        return anagram.values()
+        return ans   
