a554a2595783b2f8d45e2fdc5cf661d9bcb6c1e2
Mussie <90728686+Mussie7@users.noreply.github.com>
2022-04-19 17:12:25 +0300

    Update maximum number of coins you can get.py

--- DQ/maximum number of coins you can get.py
+++ DQ/maximum number of coins you can get.py
@@ -3,7 +3,7 @@ class Solution:
         piles.sort()
         max_coin = 0
         for i in range(len(piles)//3):
-            pile_tuple = (piles.pop(-1), piles.pop(-1), piles.pop(0))
+            pile_tuple = (piles.pop(), piles.pop(), piles.pop(0))
             max_coin += pile_tuple[1]
         
         return max_coin
